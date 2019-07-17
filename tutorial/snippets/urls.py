0e5200e9647d496184b1169bda97c8efc7a98aa6
Halifay <mihail52sh@mail.ru>
2019-07-17 14:43:12 +1000

    end of tutorial

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
-from . import views
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+from . import views
 
-urlpatterns =[
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('', views.api_view),
-    path('api-auth/', include('rest_framework.urls')),
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
 
